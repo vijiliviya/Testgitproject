25620f10d87888325b126022b962b20272e8bf95
vijiliviya <tempaauser@gmail.com>
2019-06-04 17:49:38 +0530

    newcode

---
+++ src/test/java/SamplePack/hello.java
@@ -0,0 +1,42 @@
+package SamplePack;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.AfterClass;
+	import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.Test;
+
+	/**
+	 * Hello world!
+	 *
+	 */
+	public class hello 
+	{
+		public WebDriver driver;
+		 
+		  @Test
+		  public void openMyBlog() {
+		 driver.get("https://www.softwaretestingmaterial.com/");
+		  }
+		  
+		  @BeforeClass
+		  public void beforeClass() {
+		   
+		   System.setProperty("webdriver.chrome.driver", "C:\\Users\\vraja\\workspace\\SampleMavenProject\\Driver\\chromedriver.exe");
+		   driver = new ChromeDriver();
+		   
+		  }
+		 
+		  @AfterClass
+		  public void afterClass() {
+		   //driver.quit();
+			  System.out.println("afterclass");
+			System.out.println("new");
+			  System.out.println("example");
+			System.out.println("new");
+			  System.out.println("example");
+			System.out.println("newqq");
+			  
+		  }
+	}
+
+
