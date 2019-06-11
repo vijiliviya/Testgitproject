79d7f65b14adcad2142e4d073bd5ee249ffd6524
vijiliviya <tempaauser@gmail.com>
2019-06-11 11:31:18 +0530

    New one project code
    New one project code description

--- src/main/java/Samplegit/Samplegit/App.java
+++ src/main/java/Samplegit/Samplegit/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("dear java");
             System.out.println("dear java2");
+           System.out.println("what is this");
+           System.out.println("New project allocated");
     }
 }
