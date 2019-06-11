2efe358d313f02ffa85af1992db792203202d89a
vijiliviya <tempaauser@gmail.com>
2019-06-11 10:45:48 +0530

    git hub desktop comments
    git hub desktop comments

--- src/main/java/Samplegit/Samplegit/App.java
+++ src/main/java/Samplegit/Samplegit/App.java
@@ -4,10 +4,11 @@ package Samplegit.Samplegit;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("dear java");
     }
 }
